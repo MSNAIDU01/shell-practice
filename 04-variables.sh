c36f0822294af9e71be30cf7a79fb91555dfa5b0
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-06 13:53:58 +0530

    Hello world

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:Hello $PERSON2 ,How are you??"
+echo "$PERSON2:i am good $PERSON1, how about you??"
+echo "$PERSON1:whre are you $PERSON2?"
+echo "$PERSON2: at home $PERSON1"
