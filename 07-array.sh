e0152f2830ff0b6a252ddb271bb14e6981a60feb
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-06 19:59:32 +0530

    datatypes

--- 07-array.sh
+++ 07-array.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 Names=("MS" "Killer" "king" "nag" "fish")
 echo "first name : ${Names[0]}"
+echo "first name : ${Names[1]}"
+echo "first name : ${Names[3]}"
+echo "first name : ${Names[5]}"
 
 echo "All names : ${Names[@]}"
