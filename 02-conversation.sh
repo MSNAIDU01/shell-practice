eabcbfbe07d7ef8408ceb5d21336c93bbf22d33f
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-06 13:38:50 +0530

    Hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSON1=MS
 PERSON2=Mr
-echo "$PERSON1:Hello ,How are you??"
-echo "$PERSON2:i am good, how about you??"
-echo "$PERSON1:whre are you?"
-echo "$PERSON2: at home"
+echo "MS:Hello ,How are you??"
+echo "Mr:i am good, how about you??"
+echo "MS:whre are you?"
+echo "Mr: at home"
---
+++ 3-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=MS
+PERSON2=Mr
+echo "$PERSON1:Hello $PERSON2 ,How are you??"
+echo "$PERSON2:i am good $PERSON1, how about you??"
+echo "$PERSON1:whre are you $PERSON2?"
+echo "$PERSON2: at home $PERSON1"
