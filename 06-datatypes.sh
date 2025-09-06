80fb674369d46951318d751540eaf7831d7c24ea
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-06 19:40:04 +0530

    Hello world

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 Number1=100
 Number2=200
+TIMESTAMP=$(date)
+echo "Executed at:$TIMESTAMP"
 SUM=$((Number1+Number2))
-echo"sum of $Number1 and $Number2 : $SUM"
+echo "sum of $Number1 and $Number2 : $SUM"
