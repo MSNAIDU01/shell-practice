461ccb623403d6612b3ff129fd02d031185296ea
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-06 20:02:18 +0530

    datatypes

--- 07-array.sh
+++ 07-array.sh
@@ -7,6 +7,6 @@ echo "Script print :$TIMESTAMP"
 echo "first name : ${Names[0]}"
 echo "first name : ${Names[1]}"
 echo "first name : ${Names[3]}"
-echo "first name : ${Names[5]}"
+echo "first name : ${Names[4]}"
 
 echo "All names : ${Names[@]}"
