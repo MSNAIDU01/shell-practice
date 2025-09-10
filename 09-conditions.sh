34aadad8cef8d3e2aa715922da0471147fe3f2b8
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-10 11:34:23 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,12 +2,7 @@
 
 NUMBER=$1
 
-# -gt --> greater than
-# -lt --> less than
-# -eq --> equal
-# -ne --> not equal
-
-if [ $NUMBER -lt 10 ]
+if [ $NUMBER > 10]
 then
     echo "Given number $NUMBER is less than 10"
 else
