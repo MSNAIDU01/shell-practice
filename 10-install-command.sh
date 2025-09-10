bdc44ad4afc2f307034aa673ab8261e75b73ce8c
MSNAIDU01 <msnaidu0102@gmail.com>
2025-09-10 12:05:21 +0530

    conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -3,20 +3,25 @@
 USERID=$(id -u)
 if[ $USERIF -ne 0 ]
 then 
-     echo"Error :: please run this script without root access"
+     echo "erorr  :: pleaserun this is script without root access"
      exit 1
  else 
   echo "running with root access"
 
 fi
-dnf install mysql -y
-if [ $? -eq 0 ]
-then    
-    echo "insatlling mysql is succcess"
-else
-    echo "Insatlling mysql is failure"
-    exit1
-fi
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+    then
+        echo "MySQL is not installed... going to install it"
+        dnf install mysql -y
+    if [ $? -eq 0 ]
+    then    
+        echo "insatlling mysql is succcess"
+    else
+        echo "Insatlling mysql is failure"
+        exit1
+    fi
 else
     echo "mysql already insstlled ----nothing to do"
 fi
